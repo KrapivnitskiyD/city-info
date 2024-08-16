93e3213f509556cfb776b08fca3eabc8a694c4af
krapivnitskiyD <krapivnitskiy@itstep.academy>
2024-08-16 17:02:36 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './styles.css';
+
+function PlacesPhoto(props) {
+    return (
+        <div className="place-card">
+            <p>{props.place}</p>
+            <img src={props.photo} alt={props.place} />
+        </div>
+    );
+}
+
+function AboutCity() {
+    const cityInfo = {
+        city: 'Одесса',
+        country: 'Украина',
+        founded: '2 сентября 1974',
+        places: [
+            ['Потёмкинская Лестница', 'potiomkin_stairs.jpg'],
+            ['Театр Оперы и Балета', 'opera_ballet.jpg'],
+            ['Катакомбы', 'catacombs.avif']
+        ],
+        flag: 'Flag_of_Odessa.jpg'
+    };
+
+    const cityStyle = {
+        width: '80%',
+        margin: '10px auto',
+        padding: '20px',
+        textAlign: 'center'
+    };
+
+    return (
+        <div style={cityStyle} className="place-info">
+            <h1>{cityInfo.city}</h1>
+            <img className="flag" src={cityInfo.flag} alt={cityInfo.flag} />
+            <p><strong>Страна: </strong> {cityInfo.country}</p>
+            <p><strong>Город основан: </strong> {cityInfo.founded}</p>
+            <h3>Интересные места и достопримечательности</h3>
+
+            {cityInfo.places.map((place, index) => (
+                <PlacesPhoto key={index} place={place[0]} photo={place[1]}></PlacesPhoto>
+            ))}
+        </div>
+    );
+
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AboutCity></AboutCity>
+        </div>
+    );
 }
 
 export default App;
